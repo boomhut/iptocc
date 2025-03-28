2d4ea47350e8db93dea5039982f8a643c9a44fda
boomhut <56619040+boomhut@users.noreply.github.com>
2025-03-28 05:17:45 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,20 +1,23 @@
 package iptocc
 
 import (
+	"encoding/gob"
 	"fmt"
 	"net"
 	"os"
 	"strings"
 	"sync"
+	"time"
 
 	"github.com/ip2location/ip2location-go/v9"
 )
 
 type Ip2LocationDataFiles struct {
-	DataFolder string // data folder path to store IP2Location data files. Example: /path/to/data/
-	SubFolder  string // sub folder path to store IP2Location data files. Example: /path/to/data/september2023/
-	IPv4       string // IPv4 data file name. Example: IP2LOCATION-LITE-DB11.BIN
-	IPv6       string // IPv6 data file name. Example: IP2LOCATION-LITE-DB11.IPV6.BIN
+	DataFolder string    // data folder path to store IP2Location data files. Example: /path/to/data/
+	SubFolder  string    // sub folder path to store IP2Location data files. Example: /path/to/data/september2023/
+	LastUpdate time.Time // last update time of sub folder.
+	IPv4       string    // IPv4 data file name. Example: IP2LOCATION-LITE-DB11.BIN
+	IPv6       string    // IPv6 data file name. Example: IP2LOCATION-LITE-DB11.IPV6.BIN
 	mu         sync.RWMutex
 }
 
@@ -30,8 +33,8 @@ func init() {
 	ip4DB = nil // set default IPv4 database to nil
 	ip6DB = nil // set default IPv6 database to nil
 
-	SetDataFolder("./data/ip2location") // set the data folder to the default value
-	// TODO: get the data folder from the environment variable or command line argument
+	SetDataFolder("./data/ip2location")
+	loadConfig()
 
 }
 
@@ -91,7 +94,156 @@ func SetDataFolder(dataFolder string) error {
 
 	ip2loc.DataFolder = dataFolder
 
-	i4, i6 := FindDataFiles()
+	return nil
+
+}
+
+func loadConfig() error {
+	// lock struct for write
+	ip2loc.mu.Lock()
+	defer ip2loc.mu.Unlock()
+	// check if data folder is empty
+	if ip2loc.DataFolder == "" {
+		return fmt.Errorf("data folder is empty")
+	}
+	// check if data folder is a directory
+	if fi, err := os.Stat(ip2loc.DataFolder); err != nil || !fi.IsDir() {
+		return fmt.Errorf("data folder is not a directory: %s", ip2loc.DataFolder)
+	}
+
+	// check if data folder is writable
+	if err := os.MkdirAll(ip2loc.DataFolder, 0755); err != nil {
+		return fmt.Errorf("data folder is not writable: %s", ip2loc.DataFolder)
+	}
+
+	// check if config file exists (ip2l_config.bin)
+	if _, err := os.Stat(ip2loc.DataFolder + "/ip2l_config.bin"); os.IsNotExist(err) {
+		// create config file
+		f, err := os.Create(ip2loc.DataFolder + "/ip2l_config.bin")
+		if err != nil {
+			return fmt.Errorf("error creating config file: %s", err)
+		}
+		defer f.Close()
+		// write config file. config file is a gob file containing the ip2location data files
+		// Ip2LocationDataFiles is a struct containing the data folder, sub folder, IPv4 and IPv6 data files
+		// Gob encode the Ip2LocationDataFiles struct and write it to the config file
+		enc := gob.NewEncoder(f)
+		err = enc.Encode(ip2loc)
+		if err != nil {
+			return fmt.Errorf("error encoding config file: %s", err)
+		}
+	} else {
+		// read config file
+		f, err := os.Open(ip2loc.DataFolder + "/ip2l_config.bin")
+		if err != nil {
+			return fmt.Errorf("error opening config file: %s", err)
+		}
+		defer f.Close()
+		// decode config file. config file is a gob file containing the ip2location data files
+		// Ip2LocationDataFiles is a struct containing the data folder, sub folder, IPv4 and IPv6 data files
+		// Gob decode the Ip2LocationDataFiles struct and read it from the config file
+		dec := gob.NewDecoder(f)
+		err = dec.Decode(ip2loc)
+		if err != nil {
+			return fmt.Errorf("error decoding config file: %s", err)
+		}
+
+	}
+
+	return nil
+}
+
+// OnExit function to close the database connections and save the config file
+func OnExit() {
+	// close the database connections
+	if ip4DB != nil {
+		ip4DB.Close()
+		ip4DB = nil
+	}
+	if ip6DB != nil {
+		ip6DB.Close()
+		ip6DB = nil
+	}
+
+	// save the config file
+	if err := saveConfig(); err != nil {
+		fmt.Println(err)
+		return
+	}
+}
+
+// saveConfig function to save the config file (ip2l_config.bin) to the data folder
+func saveConfig() error {
+	// check if data folder is empty
+	if ip2loc.DataFolder == "" {
+		return fmt.Errorf("data folder is empty")
+	}
+
+	// check if data folder is a directory
+	if fi, err := os.Stat(ip2loc.DataFolder); err != nil || !fi.IsDir() {
+		return fmt.Errorf("data folder is not a directory: %s", ip2loc.DataFolder)
+	}
+
+	// check if data folder is writable
+	if err := os.MkdirAll(ip2loc.DataFolder, 0755); err != nil {
+		return fmt.Errorf("data folder is not writable: %s", ip2loc.DataFolder)
+	}
+
+	// save config file. config file is a gob file containing the ip2location data files
+	// Ip2LocationDataFiles is a struct containing the data folder, sub folder, IPv4 and IPv6 data files
+	// Gob encode the Ip2LocationDataFiles struct and write it to the config file
+	f, err := os.Create(ip2loc.DataFolder + "ip2l_config.bin")
+	if err != nil {
+		return fmt.Errorf("error creating config file: %s", err)
+	}
+
+	defer f.Close()
+	enc := gob.NewEncoder(f)
+	err = enc.Encode(ip2loc)
+	if err != nil {
+		return fmt.Errorf("error encoding config file: %s", err)
+	}
+	return nil
+
+}
+
+// Function to set IP2Location data folder
+func SetSubFolder(dataFolder string) error {
+	// check if data folder exists
+	if _, err := os.Stat(dataFolder); os.IsNotExist(err) {
+		return fmt.Errorf("data folder does not exist: %s", dataFolder)
+	}
+
+	// if ip2loc is nil, create a new instance
+	if ip2loc == nil {
+		ip2loc = new(Ip2LocationDataFiles)
+	}
+	// check if data folder is empty
+	if dataFolder == "" {
+		return fmt.Errorf("data folder is empty")
+	}
+	// check if data folder is a directory
+	if fi, err := os.Stat(dataFolder); err != nil || !fi.IsDir() {
+		return fmt.Errorf("data folder is not a directory: %s", dataFolder)
+	}
+
+	// check if data folder is writable
+	if err := os.MkdirAll(dataFolder, 0755); err != nil {
+		return fmt.Errorf("data folder is not writable: %s", dataFolder)
+	}
+
+	ip2loc.mu.Lock()
+	defer ip2loc.mu.Unlock()
+
+	i4, i6, err := FindDataFiles(dataFolder)
+	if err != nil {
+		return fmt.Errorf("error finding data files: %s", err)
+	}
+
+	// set the data folder path to store IP2Location data files
+	ip2loc.SubFolder = dataFolder
+
+	// set the sub folder path to store IP2Location data files
 	ip2loc.IPv4 = i4
 	ip2loc.IPv6 = i6
 
@@ -102,15 +254,43 @@ func SetDataFolder(dataFolder string) error {
 	return nil
 }
 
+// updateSubFolder function to update the sub folder path to store IP2Location data files
+func updateSubFolder(dataFolder string) error {
+	// check if data folder exists
+	if _, err := os.Stat(dataFolder); os.IsNotExist(err) {
+		return fmt.Errorf("data folder does not exist: %s", dataFolder)
+	}
+
+	// if ip2loc is nil, create a new instance
+	if ip2loc == nil {
+		ip2loc = new(Ip2LocationDataFiles)
+	}
+	// check if data folder is empty
+	if dataFolder == "" {
+		return fmt.Errorf("data folder is empty")
+	}
+	// check if data folder is a directory
+	if fi, err := os.Stat(dataFolder); err != nil || !fi.IsDir() {
+		return fmt.Errorf("data folder is not a directory: %s", dataFolder)
+	}
+
+	ip2loc.mu.Lock()
+	// set the sub folder path to store IP2Location data files
+	ip2loc.SubFolder = dataFolder
+	ip2loc.LastUpdate = time.Now()
+	ip2loc.mu.Unlock()
+
+	return nil
+}
+
 // function to automatically find the IP2Location data files in the data folder
-func FindDataFiles() (string, string) {
+func FindDataFiles(subFolder string) (string, string, error) {
 	var ipv4, ipv6 string
 	// first find the IPv6 data file (ending with .IPV6.BIN)
 	// loop through all files in the data folder
-	files, err := os.ReadDir(ip2loc.DataFolder)
+	files, err := os.ReadDir(subFolder)
 	if err != nil {
-		fmt.Println(err)
-		return "", ""
+		return "", "", fmt.Errorf("error reading data folder: %s", err)
 	}
 	for _, file := range files {
 		if strings.HasSuffix(file.Name(), ".IPV6.BIN") {
@@ -128,7 +308,12 @@ func FindDataFiles() (string, string) {
 		}
 	}
 
-	return ipv4, ipv6
+	// check if both files are found
+	if ipv4 == "" || ipv6 == "" {
+		return "", "", fmt.Errorf("data files not found in data folder: %s", ip2loc.SubFolder)
+	}
+
+	return ipv4, ipv6, nil
 }
 
 // Function to lookup country by IP address
